0d95539ef9116273ccdcf70431991515d8f03558
cox101 <104098884+cox101@users.noreply.github.com>
2025-05-18 13:03:37 +0300

    Initial commit

--- settings.py
+++ settings.py
@@ -1,16 +1,9 @@
 import environ
-import os
 
 env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('MYSQL_DATABASE'),
-        'USER': env('MYSQL_USER'),
-        'PASSWORD': env('MYSQL_PASSWORD'),
-        'HOST': env('MYSQL_HOST'),
-        'PORT': env('MYSQL_PORT', default='3306'),
     }
 }
--- urls.py
+++ urls.py
@@ -6,16 +6,13 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="ALX Travel API",
         default_version='v1',
         description="API documentation for ALX Travel App",
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
+        public=True,
+        permission_classes=(permissions.AllowAny,),
+    )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('api/', include('listings.urls')),  # create this later
 ]
